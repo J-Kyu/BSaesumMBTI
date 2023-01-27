fb9e3c892df92722cfb90e17f7e17ceb87184a2f
j-kyu <skjeon96@gmail.com>
2023-01-27 18:29:11 +0900

    - init

---
+++ src/main/java/com/h154/saesumMBTI/SaesumMbtiApplication.java
@@ -0,0 +1,13 @@
+package com.h154.saesumMBTI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaesumMbtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaesumMbtiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/h154/saesumMBTI/SaesumMbtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h154.saesumMBTI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaesumMbtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
