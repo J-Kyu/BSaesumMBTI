4fc6e5cffad9fcaa32bd52262a8952c43eba4408
j-kyu <skjeon96@gmail.com>
2023-02-13 11:20:51 +0900

    - thymeleaf 추가 (admin page 만들어보자)

--- src/main/java/com/h154/saesumMBTI/Controller/HomeController.java
+++ src/main/java/com/h154/saesumMBTI/Controller/HomeController.java
@@ -11,16 +11,17 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @Slf4j
 public class HomeController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(){
         log.info("Home Controller");
-        return "<h1> this is home</h1>";
+        return "home";
     }
 
 
