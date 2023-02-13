10211ebbb3bb58153b24a675e9f351b208619c7d
j-kyu <skjeon96@gmail.com>
2023-02-13 13:46:53 +0900

    Revert "- thymeleaf 추가 (admin page 만들어보자)"
    This reverts commit 4fc6e5cffad9fcaa32bd52262a8952c43eba4408.

--- src/main/java/com/h154/saesumMBTI/Controller/HomeController.java
+++ src/main/java/com/h154/saesumMBTI/Controller/HomeController.java
@@ -11,17 +11,16 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @Slf4j
 public class HomeController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(){
         log.info("Home Controller");
-        return "home";
+        return "<h1> this is home</h1>";
     }
 
 
